93f8184415a078ea5df7fa764a9e5b039dec3576
pc <crunchydeer30@gmail.com>
2024-02-11 23:00:54 +0300

    remove example code from the main branch

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 import errorHandler from './middleware/errorHandler';
-import rateLimiter from './middleware/rateLimiter';
 
 import apiRouter from './router';
 
@@ -12,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('common'));
-app.use(rateLimiter);
 
 app.use('/api', apiRouter);
 
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import healthCheckRouter from './routes/healthcheck.routes';
-import postRouter from './routes/posts.routes';
 
 const apiRouter = Router();
 
 apiRouter.use('/healthcheck', healthCheckRouter);
-apiRouter.use('/posts', postRouter);
 
 export default apiRouter;
