052535f869663a9c68c52a8bbf53b5abf9a896df
PC <crunchydeer30@gmail.com>
2024-06-07 16:32:17 +0300

    remove unnecessary .*ignore files and prisma config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import env from './config/env';
 
 app.listen(env.PORT, () => {
   console.log(
-    `Server running at http://localhost:${env.PORT} in ${env.NODE_ENV} mode`,
+    '\x1b[36m%s\x1b[0m',
+    `\n\n🚀 Server running at http://localhost:${env.PORT} in ${env.NODE_ENV} mode 🚀`,
   );
 });
